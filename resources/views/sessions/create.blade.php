f7f8a4a8c41cbc27e90122272093c9d865853988
Patrick-neit <patrickaguilar2403@gmail.com>
2022-02-14 22:49:33 -0400

    Diseño Advance

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">New Session</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                           @if ($errors->any())
+                           <div class="alert alert-info alert-dismissible fade show" role="alert">
+                               <strong>Revise sus datos</strong>
+                               @foreach ($errors->all() as $error)
+                                   <span class="badge badge-success">{{$error}}</span>
+                               @endforeach
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                           </div>
+
+                           @endif
+
+                           {!! Form::open(array('route'=> 'sessions.store', 'method'=>'POST'))!!}
+                           <div class="row">
+                               <div class="col-xs-12 col-sm-12 col-md-12">
+                                   <div class="form-group">
+                                        <label for="titulo">Titulo</label>
+                                        {!! Form::text('titulo',null,array('class'=> 'form-control')) !!}
+                                   </div>
+
+                               </div>
+                               <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                     <label for="motivo">Motivo</label>
+                                     {!! Form::text('motivo',null,array('class'=> 'form-control')) !!}
+                                </div>
+
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                     <label for="duracion">Duracion</label>
+                                     {!! Form::text('duracion',null,array('class'=> 'form-control')) !!}
+                                </div>
+
+                            </div>
+
+
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <button type="submit" class="btn btn-primary">Guardar</button>
+                                <a class="btn btn-danger" href="{{route('sessions.index')}}">Volver</a>
+                            </div>
+
+                           </div>
+                             {!! Form::close()!!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
